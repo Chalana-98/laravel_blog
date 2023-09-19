53ace342f3c246adb2b883061fe815eacd9ffe36
Chalana Shehara <73055643+Chalana-98@users.noreply.github.com>
2023-09-19 22:20:08 +0530

    crud strat
    crud start

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[WelcomeController::class, 'index'])->name('welcome');
 
 Auth::routes();
 
